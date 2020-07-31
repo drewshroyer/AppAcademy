e41697d3cba82989f2c3250ed08612cae662cca5
drewshroyer <drewshroyer@gmail.com>
2020-07-31 16:35:04 -0500

    test and space complexities

--- W4D5/Execution Time Differences:Algorithms/anagrams.rb
+++ W4D5/Execution Time Differences:Algorithms/anagrams.rb
@@ -5,7 +5,7 @@ def first_anagram?(str1, str2)   # time complexity == O(n!)
     arr.include?(str2.split(""))
 end
 
-def second_anagram?(str1, str2)  # time comp == n^2 + m  or O(n * m^2)
+def second_anagram?(str1, str2)  # time comp == n^2  && space comp == n
     arr1 = str1.split("")
     arr2 = str2.split("")
     arr1.each_with_index do |ele, idx|
@@ -16,11 +16,11 @@ def second_anagram?(str1, str2)  # time comp == n^2 + m  or O(n * m^2)
     arr2.join.empty?
 end
 
-def third_anagram?(str1, str2)  #O(n log(n)) or O(n + m)
+def third_anagram?(str1, str2)  #O(n log(n)) space comp O(n) 
     str1.split("").sort == str2.split("").sort
 end
 
-def fourth_anagram?(str1, str2) # time comp = O(n + m)
+def fourth_anagram?(str1, str2) # time comp = O(n), space comp == O(1)
     hash1 = Hash.new(0)
     hash2 = Hash.new(0)
     str1.each_char {|char| hash1[char] += 1}
@@ -28,11 +28,23 @@ def fourth_anagram?(str1, str2) # time comp = O(n + m)
     hash1 == hash2
 end
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
-p third_anagram?("gizmo", "sally")    #=> false
-p third_anagram?("elvis", "lives")    #=> true
-p fourth_anagram?("elvis", "lives")    #=> true
-p fourth_anagram?("gizmo", "sally")    #=> false
+def other_anagram?(str1, str2)
+    return false if str1.length != str2.length
+    hash = Hash.new(0)
+    str1.each_char {|char| hash[char] += 1}
+    str2.each_char {|char| hash[char] += 1}
+    hash.values.all?(&:even?)
+end
+
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+# p fourth_anagram?("elvis", "lives")    #=> true
+# p fourth_anagram?("gizmo", "sally")    #=> false
+p other_anagram?("elvis", "lives")    #=> true
+p other_anagram?("gizmo", "sally")    #=> false
+p other_anagram?("elvis", "liveasdfasdfaaasa")    #=> false
+p other_anagram?("gizmo", "sallyasdfasdf")    #=> false
---
+++ W4D5/Execution Time Differences:Algorithms/two_sum.rb
@@ -0,0 +1,11 @@
+
+
+def two_sum?(arr, target_sum)
+
+end
+
+arr = [0, 1, 5, 7]
+
+
+p two_sum?(arr, 6) # => should be true
+p two_sum?(arr, 10) # => should be false
